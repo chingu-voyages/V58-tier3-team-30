cd9ddf5c50cbfb372bbc7416e91f387cfb2e1343
Andrew Smith <meuzishun@gmail.com>
2025-11-17 18:06:53 -0500

    Display country stats on MapPage

--- chingu-map-app/app/map/page.tsx
+++ chingu-map-app/app/map/page.tsx
@@ -1,5 +1,18 @@
-export default function MapPage() {
+import { chinguService } from '../../features/chingu/chingu.service';
+import { ChinguCountryStats } from '../../features/chingu/chingu.type';
+
+export default async function MapPage() {
+  const countries = await chinguService.getCountsByCountry();
+
   return (
-    <div>Map Page</div>
+    <div>
+      <h1>Map Page</h1>
+      {countries.map((country: ChinguCountryStats) => (
+        <div key={country.countryCode} className='flex gap-3'>
+          <p>{country.countryName} ({country.countryCode})</p>
+          <p>{country.count}</p>
+        </div>
+      ))}
+    </div>
   );
 }
